f50881998b54935744d6a0097c15bd37c1fa6760
José Lopes <evandeilton@gmail.com>
2025-02-18 09:35:22 -0300

    Fix call ui

---
+++ src/bdtdfinder/__init__.py
@@ -0,0 +1,6 @@
+from .BDTDUi import create_ui
+from .BDTDReviewer import BDTDReviewer
+from .BDTDResearchAgent import BDTDResearchAgent
+from .BDTDdownloader import PDFDownloader
+
+__all__ = ['create_ui', 'BDTDReviewer', 'BDTDResearchAgent', 'PDFDownloader']
